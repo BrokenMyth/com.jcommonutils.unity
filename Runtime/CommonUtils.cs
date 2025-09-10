4aaadd4c0885f7c9d44423a015b7204a50a8d69c
蒋琪显 <1074857361@qq.com>
2025-09-10 15:23:14 +0800

    feat: 初始化项目

---
+++ Runtime/CommonUtils.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonUtils
+{
+    public static class MathHelper
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => a - b;
+    }
+}
