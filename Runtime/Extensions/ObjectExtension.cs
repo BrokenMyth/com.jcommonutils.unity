a623192a2682791a21acd2931d91b16f401193d7
蒋琪显 <1074857361@qq.com>
2025-09-12 14:25:39 +0800

    feat: 添加扩展

---
+++ Runtime/Extensions/ObjectExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace Extensions
+{
+    public static class ObjectExtension
+    {
+        /// <summary>
+        /// 检查对象是否为null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool IsNull(this object self)
+        {
+            return self == null;
+        }
+
+        /// <summary>
+        /// 检查对象是否不为null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool IsNotNull(this object self)
+        {
+            return !self.IsNull();
+        }
+    }
+}
